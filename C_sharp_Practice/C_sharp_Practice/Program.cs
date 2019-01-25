e8c8d5debb210f088c73d69e1871f80b369c64b1
petros-b <pbesieri@umich.edu>
2019-01-24 22:18:06 -0500

    linkedList example (temp)

--- C_sharp_Practice/C_sharp_Practice/Program.cs
+++ C_sharp_Practice/C_sharp_Practice/Program.cs
@@ -10,6 +10,25 @@ namespace C_sharp_Practice
     {
         static void Main(string[] args)
         {
+            LinkedList<string> linked = new LinkedList<string>();
+            //add first 3 elem. to string
+
+            linked.AddLast("pet");
+            linked.AddLast("ali");
+            linked.AddLast("viv");
+            linked.AddLast("kenny");
+
+            //insert node b/4 2nd node. So right after the first one
+
+            LinkedListNode<string> node = linked.Find("pet");
+            linked.AddAfter(node, "dumb-fuck"); // finds node pet, then adds it AFTER it
+
+            foreach(var value in linked)
+            {
+                Console.WriteLine(value);
+                
+            }
+            Console.ReadKey();
         }
     }
 }
